0fbd3e1bbd7ad7e2dbd634dff3522bfef63dca19
Lyr01 <79974123+Lyr01@users.noreply.github.com>
2022-12-07 20:34:05 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			We're launching soon Days Hours Minutes Seconds
+			<div className="attribution">
+				Challenge by{" "}
+				<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+					Frontend Mentor
+				</a>
+				. Coded by <a href="https://lyr01.github.io/">Hamza Khan</a>.
+			</div>
+		</div>
+	);
+}
+
+export default App;
